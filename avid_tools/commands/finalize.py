8c7094995752f0319b7cbf18cc2695d13b650b5b
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-09-11 12:56:07 +0200

    finalize:cmd_finalize - update md5 of docIndex before generating file index

--- avid_tools/commands/finalize.py
+++ avid_tools/commands/finalize.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from click import command
 
 from avid_tools.database import create_database
+from avid_tools.database import update_md5
 from avid_tools.indices import generate_doc_index
 from avid_tools.indices import generate_file_index
 from avid_tools.utils import argument_avid_dir
@@ -16,4 +17,5 @@ def cmd_finalize(avid_dir: Path):
     conn = create_database(db_path)
     avid = AVID(avid_dir)
     generate_doc_index(conn, avid)
+    update_md5(conn, avid.indices.docIndex)
     generate_file_index(conn, avid)
