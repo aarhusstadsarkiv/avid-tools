8be9566abc1e70d428b4cb3b8e7861079c088829
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-09-12 10:40:26 +0200

    commands.init:cmd_init - use AVID, validate indices against schemata

--- avid_tools/commands/init.py
+++ avid_tools/commands/init.py
@@ -11,6 +11,7 @@ from avid_tools.indices import save_file_index
 from avid_tools.utils import argument_avid_dir
 from avid_tools.utils import AVID
 from avid_tools.utils import ctx_params
+from avid_tools.utils import validate_xml
 
 
 @command("init", no_args_is_help=True)
@@ -18,6 +19,7 @@ from avid_tools.utils import ctx_params
 @pass_context
 def cmd_init(ctx: Context, avid_dir: Path):
     db_path: Path = avid_dir.joinpath("_metadata", "avid.db")
+    avid = AVID(avid_dir)
 
     if db_path.is_file():
         raise BadParameter(f"_metadata/avid.db already exists for {avid_dir.name}.", ctx, ctx_params(ctx)["avid_dir"])
@@ -25,16 +27,27 @@ def cmd_init(ctx: Context, avid_dir: Path):
     db_path.parent.mkdir(parents=True, exist_ok=True)
 
     for index_file in (
-        "archiveIndex.xml",
-        "contextDocumentationIndex.xml",
-        "docIndex.xml",
-        "fileIndex.xml",
-        "tableIndex.xml",
+        avid.indices.archiveIndex,
+        avid.indices.contextDocumentationIndex,
+        avid.indices.docIndex,
+        avid.indices.fileIndex,
+        avid.indices.tableIndex,
     ):
-        if not avid_dir.joinpath("Indices", index_file).is_file():
-            raise BadParameter(f"Missing Indices/{index_file}", ctx, ctx_params(ctx)["avid_dir"])
+        if not index_file.is_file():
+            raise BadParameter(f"missing Indices/{index_file.name}", ctx, ctx_params(ctx)["avid_dir"])
+
+    for index_file, schema in (
+        (avid.indices.archiveIndex, avid.schemas.archiveIndex),
+        (avid.indices.contextDocumentationIndex, avid.schemas.contextDocumentationIndex),
+        (avid.indices.tableIndex, avid.schemas.tableIndex),
+    ):
+        if validation_error := validate_xml(index_file, schema):
+            raise BadParameter(
+                f"error in Indices/{index_file.name}, {validation_error.msg}",
+                ctx,
+                ctx_params(ctx)["avid_dir"],
+            )
 
     conn = create_database(db_path)
-    avid = AVID(avid_dir)
     save_file_index(conn, avid)
     save_doc_index(conn, avid)
