c92639d2598829115653a5405f9c8ed4f6724dbe
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-09-12 14:30:19 +0200

    commands.search:cmd_search - allow to specify columns to search in

--- avid_tools/commands/search.py
+++ avid_tools/commands/search.py
@@ -22,48 +22,63 @@ from avid_tools.utils import ctx_params
 
 
 class ContentHandlerTableSearch(ContentHandler):
-    def __init__(self, regexp: Pattern[str], on_match: Callable[[int, list[str]], None], limit: int | None = None):
+    def __init__(
+        self,
+        regexp: Pattern[str],
+        on_match: Callable[[int, dict[int, str]], None],
+        limit: int | None = None,
+        column_ids: list[int] | None = None,
+    ):
         super().__init__()
         self.regexp: Pattern[str] = regexp
-        self.on_match: Callable[[int, list[str]], None] = on_match
+        self.on_match: Callable[[int, dict[int, str]], None] = on_match
         self.limit: int | None = limit
+        self.column_ids: list[int] | None = column_ids
         self.is_row: bool = False
-        self.is_column: bool = False
         self.row_number: int = 0
-        self.row_columns: list[str] = []
+        self.column_id: int | None = None
+        self.row_columns: dict[int, str] = {}
         self.matches: int = 0
 
     def startElement(self, name: str, attrs: AttributesImpl):
         if name == "row":
-            self.is_row: bool = True
+            self.is_row = True
             self.row_number += 1
         elif self.is_row and name[0] == "c":
-            self.is_column = True
-            self.row_columns.append("")
+            column_id: int = int(name[1:])
+            if not self.column_ids or column_id in self.column_ids:
+                self.column_id = column_id
+                self.row_columns[column_id] = ""
 
     def characters(self, content: str):
-        if self.is_column:
-            self.row_columns[-1] += content
+        if self.column_id is not None:
+            self.row_columns[self.column_id] += content
 
     def endElement(self, name: str):
         if name == "row":
-            if any(self.regexp.search(c) for c in self.row_columns):
+            if any(self.regexp.search(c) for c in self.row_columns.values()):
                 self.on_match(self.row_number, self.row_columns)
                 self.matches += 1
                 if self.limit and self.matches >= self.limit:
                     raise StopIteration
             self.is_row = False
-            self.is_column = False
-            self.row_columns: list[str] = []
-        elif self.is_column and name[0] == "c":
-            self.is_column = False
+            self.row_columns = {}
+        elif self.column_id is not None and name[0] == "c":
+            self.column_id = None
 
 
 @command("search", no_args_is_help=True)
 @argument_avid_dir(True)
 @argument("patterns", metavar="PATTERN...", nargs=-1, required=True)
 @option("--table", "-t", "table_ids", metavar="ID", type=IntRange(min=1), multiple=True)
-@option("--limit", type=IntRange(1), default=None)
+@option(
+    "--column",
+    "columns",
+    metavar="TABLE_ID COLUMN_ID",
+    type=(IntRange(1), IntRange(1)),
+    multiple=True,
+)
+@option("--limit", metavar="INTEGER", type=IntRange(1), default=None)
 @option("--show-columns/--show-rows", "show_columns", is_flag=True, default=True)
 @pass_context
 def cmd_search(
@@ -71,6 +86,7 @@ def cmd_search(
     avid_dir: Path,
     patterns: tuple[str, ...],
     table_ids: tuple[int, ...],
+    columns: tuple[tuple[int, int], ...],
     limit: int | None,
     show_columns: bool,
 ):
@@ -79,31 +95,41 @@ def cmd_search(
 
     if invalid_ids := [i for i in table_ids if i not in tables]:
         raise BadParameter(f"no tables with ID {', '.join(map(str, invalid_ids))}", ctx, ctx_params(ctx)["table_ids"])
+    if invalid_ids := [i for i, _ in columns if i not in tables]:
+        raise BadParameter(f"no tables with ID {', '.join(map(str, invalid_ids))}", ctx, ctx_params(ctx)["columns"])
 
     patterns = [p_.replace("%", ".*").replace("_", ".?") for p in patterns if (p_ := p.strip())]
     pattern = re_compile("(" + "|".join(patterns) + ")", IGNORECASE)
 
-    table_ids = table_ids or sorted(tables.keys())
+    targets: dict[int, list[int] | None] = {}
+
+    for table_id in table_ids:
+        targets[table_id] = None
+    for [table_id, column_id] in columns:
+        targets[table_id] = targets.get(table_id) or []
+        targets[table_id].append(column_id)
+
+    targets = targets or {i: None for i in sorted(tables.keys())}
 
     if show_columns:
 
-        def on_match_print(table: int, row_number: int, columns: list[str]):
+        def on_match_print(_table: int, _row_number: int, _columns: dict[int, str]):
             print(
-                *(f"table{table_id}/row{row_number}/c{n}: {col}" for n, col in enumerate(columns, 1)),
+                *(f"table{_table}/row{_row_number}/c{n}: {col}" for n, col in _columns.items()),
                 sep="\n",
                 end="\n\n",
             )
     else:
 
-        def on_match_print(_table: int, row_number: int, _columns: list[str]):
-            print(f"table{table_id}/row{row_number}")
+        def on_match_print(_table: int, _row_number: int, _columns: dict[int, str]):
+            print(f"table{_table}/row{_row_number}")
 
-    for table_id in table_ids:
+    for table_id, column_ids in targets.items():
         file: Path = tables[table_id]
         on_match = lambda r, cs: on_match_print(table_id, r, cs)
 
         with file.open("r") as fh:
-            handler = ContentHandlerTableSearch(pattern, on_match, limit)
+            handler = ContentHandlerTableSearch(pattern, on_match, limit, column_ids)
             try:
                 sax_parse(fh, handler)
             except StopIteration:
