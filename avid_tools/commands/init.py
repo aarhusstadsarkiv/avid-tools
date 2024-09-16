e708b877f3785f43d1089e13c1706e164b74cf10
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-09-16 10:16:29 +0200

    init:cmd_init - use optional AVID_DIR argument

--- avid_tools/commands/init.py
+++ avid_tools/commands/init.py
@@ -1,21 +1,27 @@
 from pathlib import Path
 
+from click import argument
 from click import BadParameter
 from click import command
 from click import Context
 from click import pass_context
+from click import Path as ClickPath
 
 from avid_tools.database import create_database
 from avid_tools.indices import save_doc_index
 from avid_tools.indices import save_file_index
-from avid_tools.utils import argument_avid_dir
 from avid_tools.utils import AVID
 from avid_tools.utils import ctx_params
 from avid_tools.utils import validate_xml
 
 
-@command("init", no_args_is_help=True)
-@argument_avid_dir(False)
+@command("init")
+@argument(
+    "AVID_DIR",
+    type=ClickPath(exists=True, file_okay=False, writable=True, readable=True, resolve_path=True),
+    required=False,
+    callback=lambda _c, _p, v: Path(v) if v else Path.cwd(),
+)
 @pass_context
 def cmd_init(ctx: Context, avid_dir: Path):
     db_path: Path = avid_dir.joinpath("_metadata", "avid.db")
@@ -23,8 +29,6 @@ def cmd_init(ctx: Context, avid_dir: Path):
 
     if db_path.is_file():
         raise BadParameter(f"_metadata/avid.db already exists for {avid_dir.name}.", ctx, ctx_params(ctx)["avid_dir"])
-
-    db_path.parent.mkdir(parents=True, exist_ok=True)
 
     for index_file in (
         avid.indices.archiveIndex,
@@ -48,6 +52,7 @@ def cmd_init(ctx: Context, avid_dir: Path):
                 ctx_params(ctx)["avid_dir"],
             )
 
+    db_path.parent.mkdir(parents=True, exist_ok=True)
     conn = create_database(db_path)
     save_file_index(conn, avid)
     save_doc_index(conn, avid)
