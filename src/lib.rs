021f3d52b492d58d9ee713b43ff3c14421cf89cf
Kristoffer Pedersen <rosengreenpedersen@proton.me>
2026-04-03 13:04:38 +0200

    rust encode database in bloom filter for quick probabilistic searches

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,94 @@
 use pyo3::{exceptions::PyRuntimeError, prelude::*};
 
+mod encode;
 mod typeformat;
+
+fn construct_ignore_matcher(
+    ignore_tables: Option<Vec<String>>,
+    ignore_columns: Option<Vec<String>>,
+    ignore_table_regexes: Option<encode::RegexVec>,
+    ignore_column_regexes: Option<encode::RegexVec>,
+) -> encode::IgnoreMatcher {
+    encode::IgnoreMatcher {
+        ignore_tables: ignore_tables.unwrap_or(Vec::new()),
+        ignore_columns: ignore_columns.unwrap_or(Vec::new()),
+        ignore_table_regexes: ignore_table_regexes
+            .unwrap_or(encode::RegexVec(Vec::new()))
+            .into(),
+        ignore_column_regexes: ignore_column_regexes
+            .unwrap_or(encode::RegexVec(Vec::new()))
+            .into(),
+    }
+}
 
 #[pyfunction]
 fn validate_tables_xsd() -> PyResult<Vec<String>> {
     typeformat::all_tables().map_err(|e| PyRuntimeError::new_err(e.to_string()))
 }
 
+#[pyfunction]
+fn encode_database(
+    db_path: String,
+    // Args for IgnoreMatcher - Those columns or tables to ignore
+    ignore_tables: Option<Vec<String>>,
+    ignore_columns: Option<Vec<String>>,
+    ignore_table_regexes: Option<encode::RegexVec>,
+    ignore_column_regexes: Option<encode::RegexVec>,
+) {
+    let ignore = construct_ignore_matcher(
+        ignore_tables,
+        ignore_columns,
+        ignore_table_regexes,
+        ignore_column_regexes,
+    );
+    encode::create_encoded_database(db_path, &ignore);
+}
+
+#[pyfunction]
+fn search_database(
+    db_path: String,
+    search_text: String,
+
+    // Args for IgnoreMatcher - Those columns or tables to ignore
+    ignore_tables: Option<Vec<String>>,
+    ignore_columns: Option<Vec<String>>,
+    ignore_table_regexes: Option<encode::RegexVec>,
+    ignore_column_regexes: Option<encode::RegexVec>,
+) {
+    let ignore = construct_ignore_matcher(
+        ignore_tables,
+        ignore_columns,
+        ignore_table_regexes,
+        ignore_column_regexes,
+    );
+
+    encode::search_loaded(search_text, db_path, &ignore);
+}
+
+#[pyfunction]
+fn contains_database(
+    db_path: String,
+    search_text: String,
+
+    // Args for IgnoreMatcher - Those columns or tables to ignore
+    ignore_tables: Option<Vec<String>>,
+    ignore_columns: Option<Vec<String>>,
+    ignore_table_regexes: Option<encode::RegexVec>,
+    ignore_column_regexes: Option<encode::RegexVec>,
+) {
+    let ignore = construct_ignore_matcher(
+        ignore_tables,
+        ignore_columns,
+        ignore_table_regexes,
+        ignore_column_regexes,
+    );
+
+    encode::search_loaded(search_text, db_path, &ignore);
+}
+
 #[pymodule]
 fn whitespacevalidate(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(validate_tables_xsd, m)?)?;
     m.add_function(wrap_pyfunction!(validate_tables_xsd, m)?)?;
     Ok(())
 }
