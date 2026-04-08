8579d453e93bbbfd3b9df652711c44e135d346a3
Kristoffer <prik@aarhus.dk>
2026-04-08 09:13:25 +0200

    simplification of option args

--- python/avid_tools/encoding/bloom.py
+++ python/avid_tools/encoding/bloom.py
@@ -5,6 +5,20 @@ This file interfaces Rust implmented Bloom filters.
 import click
 
 
+def _ignore_args(func):
+    options = [
+        click.option("--ignore_tables", type=str, multiple=True, default=[], help="Name(s) of tables to ignore"),
+        click.option("--ignore_columns", type=str, multiple=True, default=[], help="Name(s) of columns to ignore"),
+        click.option("--ignore_table_regexes", type=str, multiple=True, default=[], help="Regex(es) of tables to ignore"),
+        click.option("--ignore_column_regexes", type=str, multiple=True, default=[], help="Regex(es) of columns to ignore")
+    ]
+
+    for option in reversed(options):
+        option(func)
+
+    return func
+
+
 @click.group("bloom")
 def grp_bloom_filter():
     """
@@ -17,10 +31,7 @@ def grp_bloom_filter():
 
 @grp_bloom_filter.command("encode")
 @click.argument("db_path", type=click.Path(exists=True, dir_okay=False))
-@click.option("--ignore_tables", type=str, multiple=True, default=[], help="Name(s) of tables to ignore")
-@click.option("--ignore_columns", type=str, multiple=True, default=[], help="Name(s) of columns to ignore")
-@click.option("--ignore_table_regexes", type=str, multiple=True, default=[], help="Regex(es) of tables to ignore")
-@click.option("--ignore_column_regexes", type=str, multiple=True, default=[], help="Regex(es) of columns to ignore")
+@_ignore_args
 def cmd_encode(db_path: str, ignore_tables: list[str], ignore_columns: list[str], ignore_table_regexes: list[str], ignore_column_regexes: list[str]):
     """
     Encode an SQLite database in bloom filters
@@ -33,10 +44,7 @@ def cmd_encode(db_path: str, ignore_tables: list[str], ignore_columns: list[str]
 @grp_bloom_filter.command("search")
 @click.argument("db_path", type=click.Path(exists=True, dir_okay=False))
 @click.argument("text", type=str)
-@click.option("--ignore_tables", type=str, multiple=True, default=[], help="Name(s) of tables to ignore")
-@click.option("--ignore_columns", type=str, multiple=True, default=[], help="Name(s) of columns to ignore")
-@click.option("--ignore_table_regexes", type=str, multiple=True, default=[], help="Regex(es) of tables to ignore")
-@click.option("--ignore_column_regexes", type=str, multiple=True, default=[], help="Regex(es) of columns to ignore")
+@_ignore_args
 def cmd_search(db_path: str, text: str, ignore_tables: list[str], ignore_columns: list[str], ignore_table_regexes: list[str], ignore_column_regexes: list[str]):
     """
     Search an SQLite database encoded in bloom filters
@@ -48,10 +56,7 @@ def cmd_search(db_path: str, text: str, ignore_tables: list[str], ignore_columns
 
 @grp_bloom_filter.command("contains")
 @click.argument("text", type=str)
-@click.option("--ignore_tables", type=str, multiple=True, default=[], help="Name(s) of tables to ignore")
-@click.option("--ignore_columns", type=str, multiple=True, default=[], help="Name(s) of columns to ignore")
-@click.option("--ignore_table_regexes", type=str, multiple=True, default=[], help="Regex(es) of tables to ignore")
-@click.option("--ignore_column_regexes", type=str, multiple=True, default=[], help="Regex(es) of columns to ignore")
+@_ignore_args
 def cmd_contains(text: str, ignore_tables: list[str], ignore_columns: list[str], ignore_table_regexes: list[str], ignore_column_regexes: list[str]):
     """
     Test if text string may be contained in an SQLite database encoded in bloom filters
