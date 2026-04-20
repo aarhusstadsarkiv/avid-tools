fa54009dd76bfaae30df41ae50a30e491d547871
Kristoffer <prik@aarhus.dk>
2026-04-20 10:08:29 +0200

    add handler for version control handling and primary key adder

--- python/avid_tools/utils.py
+++ python/avid_tools/utils.py
@@ -112,7 +112,11 @@ class AVID:
 
     @property
     def tables(self) -> dict[int, Path]:
-        """Tables"""
+        """Tables
+
+        Returns:
+            dict: table id as key, Path to table as value
+        """
         return {
             int(f.name.removeprefix("table")): f.joinpath(f.name).with_suffix(".xml")
             for f in self.dir.joinpath("Tables").iterdir()
@@ -128,7 +132,6 @@ def find_avid_dir(path: Path, *, raise_on_error: Literal[False] = False) -> Path
 def find_avid_dir(path: Path, *, raise_on_error: bool = True) -> Path | None:
     def inner(p: Path) -> Path | None:
         pattern = r"AVID\.[A-Z]+\.\d+\..*"
-        print(p.name, p, match(pattern, p.name))
         if p.joinpath("_metadata", "avid_tools.db").is_file():
             return p
         elif match(pattern, p.name):
